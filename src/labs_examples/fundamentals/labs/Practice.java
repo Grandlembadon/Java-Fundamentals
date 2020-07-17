ee9771734cddd929593095a07630d2745b24b3f3
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-07-16 21:58:11 -0400

    Committing Lab 1 answers

---
+++ src/labs_examples/fundamentals/labs/Practice.java
@@ -0,0 +1,13 @@
+package labs_examples.fundamentals.labs;
+
+public class Practice {
+    public static void main(String[] args) {
+        String str1 = "Hello String 1!";
+        String str2 = "Hello String 2!";
+
+        System.out.println("The length of String is " + str1.length());
+    }
+}
+    String str1 = "Hello String 1!";
+    String str2 = "Hello String 2!";
+
