2d1eaa8896a5ce3a5e731dc6de01732d5961b0a3
Liam Donovan <grandlembadon@gmail.com>
2020-07-19 23:11:18 -0400

    Committing src variables change.

---
+++ src/variables/Casting.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class Casting {
+    public static void main(String[] args) {
+        double val = 23.45;
+        int intVal = (int) val;
+        System.out.println(intVal);
+    }
+}
