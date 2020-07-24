7eaff147a9c1a9a2081e49ea0c331026596bd1ad
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-07-23 22:09:29 -0400

    Committing Exercise 06 and 07 work

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,14 +13,18 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Exercise_06 {
 
     public static void main(String[] args) {
-        float r = 3.14;
+        double r = 3.14;
         int h = 5;
-        float f = h;
-        float p = 3.14;
-        float v = p * (r * r) * f;
+        int t = 2;
+        double w = t;
+        double f = h;
+        double p = 3.14;
+        double v = p * (r * r) * f;
+        double s = (w * (p * (r * r))) + ((w * p) * r * f);
         System.out.println(v);
+        System.out.println(s);
 
-        // write code here
+
 
     }
 }
