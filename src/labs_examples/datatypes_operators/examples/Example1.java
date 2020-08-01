4be6dc584fcfd98c63cb96387566265965ccaf36
Liam Donovan <grandlembadon@gmail.com>
2020-07-31 22:19:34 -0400

    commiting change to file

--- src/labs_examples/datatypes_operators/examples/Example1.java
+++ src/labs_examples/datatypes_operators/examples/Example1.java
@@ -1,20 +1,21 @@
 package labs_examples.datatypes_operators.examples;
 
-/*
-   Compute the number of cubic inches
-   in 1 cubic mile.
-*/
-class Inches {
-    public static void main(String args[]) {
-        long ci;
-        long im;
 
-        im = 5280 * 12;
 
-        ci = im * im * im;
-
-        System.out.println("There are " + ci +
-                " cubic inches in cubic mile.");
-
-    }
-}
+public static void main(String[] args) {
+        int i = 1;
+        switch(i) {
+        case 0:
+        System.out.println("i is zero");
+        case 1:
+        System.out.println("i is one");
+        case 2:
+        System.out.println("i is two");
+        case 3:
+        System.out.println("i is three");
+        case 4:
+        System.out.println("i is four");
+default :
+        System.out.println("i is not 0, 1, 2, 3 or 4");
+        }
+        }
