2b9656156db5e6ea819eb4bd288a9bb5c668cc94
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-08-02 21:26:24 -0400

    Committing work on labs

--- src/labs_examples/conditions_loops/labs/Exercise_03.java
+++ src/labs_examples/conditions_loops/labs/Exercise_03.java
@@ -1,5 +1,12 @@
 package labs_examples.conditions_loops.labs;
 
+
+
+import java.util.Scanner;
+
+
+
+
 /**
  * Conditions and Loops Exercise 3: Months of the year
  *
@@ -9,5 +16,50 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_03 {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int i = scan.nextInt();
+        switch(i){
+            case 1:
+                System.out.println("The month is January");
+                break;
+            case 2:
+                System.out.println("The month is February");
+                break;
+            case 3:
+                System.out.println("The month is March");
+                break;
+            case 4:
+                System.out.println("The month is April");
+                break;
+            case 5:
+                System.out.println("The month is May");
+                break;
+            case 6:
+                System.out.println("The month is June");
+                break;
+            case 7:
+                System.out.println("The month is July");
+                break;
+            case 8:
+                System.out.println("The month is August");
+                break;
+            case 9:
+                System.out.println("The month is September");
+                break;
+            case 10:
+                System.out.println("The month is October");
+                break;
+            case 11:
+                System.out.println("The month November");
+                break;
+            case 12:
+                System.out.println("The month is November");
+                break;
+            case 13:
+                System.out.println("The number is not a month");
+                break;
+        }
+    }
 }
