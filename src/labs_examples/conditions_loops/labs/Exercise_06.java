e834d8b52565939ce953f99191618c540f944c70
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-08-06 21:46:02 -0400

    Pushing Lab Exercise

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -8,4 +8,13 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_06 {
+    public static void main(String[] args) {
+        int num = 100, i = 1, sum = 0;
+        while(i <= num){
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum = " + sum);
+
+    }
 }
