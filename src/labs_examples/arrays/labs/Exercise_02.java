8c0d9202f0d90068cc628a5aee09e7a6e154fcf0
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-09-15 22:30:56 -0400

    committing lab work

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -9,13 +11,21 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_02 {
-
     public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
-        // write code here
-
+        System.out.println("Please enter a number between 1-10.");
+        int n = number.nextInt();
+        System.out.println("The index of your number is" + " " + array[n]);
 
     }
+
+
+
+
+
+
 }
+
+
