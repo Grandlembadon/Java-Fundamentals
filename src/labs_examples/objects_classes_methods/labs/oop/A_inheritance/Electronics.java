e96194ddd7ee902ea90694ad8be6e45fdadd5ff0
Liam Donovan <grandlembadon@gmail.com>
2020-10-19 22:15:36 -0400

    Committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Electronics.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Electronics.java
@@ -9,10 +9,43 @@ public class Electronics {
         this.brand = brand;
 
     }
+    public static int Multiply(int a, int b){
+        return a * b;
+    }
+    public static double Multiply(double a, double b){
+        return a * b;
+    }
+    public static int Multiply(int a, int b, int c){
+        return a * b * c;
+    }
+    public static int Divide(int a, int b){
+        return a / b;
+    }
+    public static double Divide(double a, double b){
+        return a / b;
+
+    }
+    public static int Add(int a, int b){
+        return a + b;
+    }
+    public static int Add(int a, int b, int c, int d){
+        return a + b + c + d;
+    }
+    public static double Add(double a, double b){
+        return a + b;
+    }
 
 }
 class VideoGameConsole extends Electronics {
     int Memory;
+    String Color;
+
+    public Vi.deoGameConsole(int Memory, String Color){
+        this.Memory = Memory;
+        this.Color = Color;
+
+    }
+
 
 
     public VideoGameConsole(boolean isWireless, String brand) {
@@ -21,6 +54,8 @@ class VideoGameConsole extends Electronics {
 }
 
 class PlayStation4 extends VideoGameConsole {
+
+
 
     public PlayStation4(boolean isWireless, String brand) {
         super(isWireless, brand);
