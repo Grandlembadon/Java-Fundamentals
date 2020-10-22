f31e35722e93767db6b77cc8253f36e7d625677f
Liam Donovan <grandlembadon@gmail.com>
2020-10-22 19:20:49 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Electronics.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Electronics.java
@@ -1,16 +1,17 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class Electronics {
-    boolean isWireless;
-    String brand;
+    private boolean isWireless;
+    private String brand;
 
+    public Electronics() {
+        isWireless = false;
+        brand = "";
+    }
     public Electronics(boolean isWireless, String brand){
         this.isWireless = isWireless;
         this.brand = brand;
 
-    }
-
-    public Electronics() {
     }
 
     public static int Multiply(int a, int b){
@@ -39,26 +40,84 @@ public class Electronics {
         return a + b;
     }
 
+    public boolean isWireless(boolean b) {
+        return isWireless;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setWireless(boolean wireless) {
+        isWireless = wireless;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
 class VideoGameConsole extends Electronics {
-    int Memory;
-    String Color;
+    private int Memory;
+    private String Color;
 
-    public VideoGameConsole(int Memory, String Color){
+    public VideoGameConsole(){
+        super();
+        Memory = 0;
+        Color = "";
+    }
+    public VideoGameConsole(boolean isWireless, String brand, int Memory, String Color){
+        super(isWireless, brand);
         this.Memory = Memory;
         this.Color = Color;
+
     }
-    
-    public VideoGameConsole(boolean isWireless, String brand) {
-        super(isWireless, brand);
+
+    public int getMemory() {
+        return Memory;
+    }
+
+    public void setMemory(int memory) {
+        Memory = memory;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
     }
 }
 
 class PlayStation4 extends VideoGameConsole {
+    private boolean PlaysBluRay;
+    private int StorageSize;
 
-
-
-    public PlayStation4(boolean isWireless, String brand) {
-        super(isWireless, brand);
+    public PlayStation4(){
+        super();
+        PlaysBluRay = true;
+        StorageSize = 0;
     }
+    public PlayStation4(boolean isWireless, String brand, int Memory, String Color, boolean PlaysBluRay, int StorageSize){
+        super(isWireless, brand, Memory, Color);
+        this.PlaysBluRay = PlaysBluRay;
+        this.StorageSize = StorageSize;
+    }
+
+    public boolean isPlaysBluRay() {
+        return PlaysBluRay;
+    }
+
+    public void setPlaysBluRay(boolean playsBluRay) {
+        PlaysBluRay = playsBluRay;
+    }
+
+    public int getStorageSize() {
+        return StorageSize;
+    }
+
+    public void setStorageSize(int storageSize) {
+        StorageSize = storageSize;
+    }
+
 }
--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/MealPlan.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/MealPlan.java
@@ -1,16 +1,39 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class MealPlan {
-    int mealnum;
-    int cost;
+    private int mealnum;
+    private int cost;
     String[] Ingredients;
 
-    public Ingredients(int mealnum, int cost, String[] Ingredients){
+    public MealPlan(int mealnum, int cost, String[] Ingredients){
         this.mealnum = mealnum;
         this.cost = cost;
         this.Ingredients = Ingredients;
     }
 
+    public int getMealnum() {
+        return mealnum;
+    }
+
+    public void setMealnum(int mealnum) {
+        this.mealnum = mealnum;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String[] getIngredients() {
+        return Ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        Ingredients = ingredients;
+    }
 }
 
 class Cuisine extends MealPlan {
