dfd7b1f15242ada5e9a8dd4b2fb72fd5963b7d8c
Liam Donovan <oblivious_peacemaker@Liams-MacBook-Pro.local>
2020-10-22 19:20:49 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Controller.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Controller.java
@@ -2,6 +2,12 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class Controller{
     public static void main(String[] args) {
+        PlayStation4 myPS4 = new PlayStation4();
+        myPS4.isWireless(true);
+        myPS4.setPlaysBluRay(true);
+        myPS4.setColor("White");
+        myPS4.
+
 
     }
 
