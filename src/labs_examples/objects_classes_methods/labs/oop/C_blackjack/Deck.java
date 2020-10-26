d6dcfc8a87eec77cceb7ecbbf54877bef9ff24fe
Liam Donovan <grandlembadon@gmail.com>
2020-10-25 22:10:30 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
@@ -1,6 +1,15 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
+
+import java.util.ArrayList;
+
 public class Deck {
     Card[] cards;
     ArrayList<Integer> usedCards;
+
+    public Deck(Card[] cards, ArrayList<Integer> usedCards){
+        this.cards = cards;
+        this.usedCards = usedCards;
+    }
+
 }
