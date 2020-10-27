0da703133c035db52f93d711f5f13778c28334cd
Liam Donovan <grandlembadon@gmail.com>
2020-10-26 22:05:33 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Hand.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Hand.java
@@ -1,6 +1,15 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
+import java.util.ArrayList;
+
 public class Hand {
+    ArrayList<Card> cards;
+    int handValue;
+
+    public void addCard(Card card){
+        this.cards.add(card);
+    }
+
 
 
     }
