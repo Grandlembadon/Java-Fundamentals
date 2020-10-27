9ff3186cc8b380591ce1df43c6def88c3a43dbf0
Liam Donovan <oblivious_peacemaker@Liams-MacBook-Pro.local>
2020-10-26 23:40:23 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
@@ -3,25 +3,32 @@ package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
 import java.util.ArrayList;
 
+import static labs_examples.objects_classes_methods.labs.oop.C_blackjack.Card.cardValues;
+
 public class Deck {
     Card[] deck;
     ArrayList<Integer> usedCards;
 
-    public Deck(Card[] cards, ArrayList<Integer> usedCards){
+    public Deck(Card[] cards, ArrayList<Integer> usedCards) {
         this.deck = cards;
         this.usedCards = usedCards;
-        deck = new Card[52];
-        int cardCt = 0;
-        for (char mySuit : suits) {
-            for (String myCardValue : cardValues) {
-                deck[cardCt] = new Card(mySuit, myCardValue);
-                cardCt++;
-            }
-
-        }
-        usedCards = new ArrayList<>();
 
 
     }
 
+    public void createDeck(){
+        deck = new Card[52];
+        int cardCt = 0;
+        for (char suit = 0; suit <= 3; suit++){
+            for (String value = cardValues[0]; value <= cardValues.length; value++;){
+                deck[cardCt] = new Card(suit, value);
+                cardCt++;
+
+            }
+        }
+
+
+    }
+
+}
 }
