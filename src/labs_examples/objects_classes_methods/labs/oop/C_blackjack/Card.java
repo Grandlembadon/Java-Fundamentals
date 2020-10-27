f91b1a0c51146aab54bba6e0f19c52e9d9d0b9fb
Liam Donovan <oblivious_peacemaker@Liams-MacBook-Pro.local>
2020-10-26 23:40:23 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Card.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Card.java
@@ -1,44 +1,60 @@
 package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
 public class Card {
-    char[] suits = {'♠', '♦', '♥', '♣'};
-    String[] cardValues = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
-    private char mySuit;
-    private String myCardValue;
+    public static char[] suits = {'♠', '♦', '♥', '♣'};
+    public static String[] cardValues = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+    private char suit;
+    private String value;
 
 
-    public Card(char mysuit, String myCardValue) {
-        this.mySuit = mysuit;
-        this.myCardValue = myCardValue;
+    public Card(char suit, String value) {
+        this.suit = suit;
+        this.value = value;
     }
 
-    public char getMySuit() {
-        return mySuit;
+    public char[] getSuits() {
+        return suits;
     }
 
-    public void setMySuit(char mySuit) {
-        this.mySuit = mySuit;
+    public void setSuits(char[] suits) {
+        this.suits = suits;
     }
 
-    public String getMyCardValue() {
-        return myCardValue;
+    public String[] getCardValues() {
+        return cardValues;
     }
 
-    public void setMyCardValue(String myCardValue) {
-        this.myCardValue = myCardValue;
+    public void setCardValues(String[] cardValues) {
+        this.cardValues = cardValues;
+    }
+
+    public char getSuit() {
+        return suit;
+    }
+
+    public void setSuit(char mySuit) {
+        this.suit = mySuit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 
     public char getSuitAsString() {
-        for (char mySuit : suits) {
-            return mySuit;
+        for (char suit : suits) {
+            return suit;
         }
 
         return 0;
     }
 
     public String getValueAsString() {
-        for (String myCardValue : cardValues) {
-            return myCardValue;
+        for (String value : cardValues) {
+            return value;
         }
         return null;
     }
