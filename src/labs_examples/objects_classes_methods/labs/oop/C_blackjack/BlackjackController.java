a229c5c1399a3426101264b04d5c645ebea4f7f9
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-10-28 21:08:05 -0400

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -2,7 +2,9 @@ package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
 public class BlackjackController {
     public static void main(String[] args) {
-        Card mycard = new Card('♠', "Ace");
-        System.out.println(mycard.toString());
+        Deck deck = new Deck;
+        deck.createDeck();
+
+
     }
 }
