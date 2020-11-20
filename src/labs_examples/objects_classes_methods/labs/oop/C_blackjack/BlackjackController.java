80a635749760f8e03804f16ccadbe8c90abbe9aa
Liam Donovan <grandlembadon@gmail.com>
2020-11-19 21:40:00 -0500

    committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -3,12 +3,19 @@ package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 import java.util.Scanner;
 
 public class BlackjackController {
+    /**
+     * This is the main method.
+     * @param args-abc
+     */
     public static void main(String[] args) {
         BlackjackController controller = new BlackjackController();
         controller.playBlackJack();
 
     }
 
+    /**
+     * This is the playBlackJack method.
+     */
     public void playBlackJack() {
 
         System.out.println("Hello player, please enter your name.");
@@ -34,33 +41,40 @@ public class BlackjackController {
             dealInitialCards(deck, user, computer);
 
             while (user.cont || computer.cont) {
-                promptUserPlay(deck, user);
-                promptComputerPlay(deck, computer);
-
-                }
-            if (user.getHand().bust() || computer.getHand().bust()) {
-                determineWinner(user, computer);
-                playAgain = promptPlayAgain();
-                firstGame = false;
-
-            } else {
-                determineWinner(user, computer);
-                playAgain = promptPlayAgain();
-                firstGame = false;
+                promptUserPlay(deck, user, computer);
+                promptComputerPlay(deck, user, computer);
 
             }
+            if (!user.getHand().bust() && !computer.getHand().bust()) {
+                determineWinner(user, computer);
+            }
+            playAgain = promptPlayAgain(user);
+            firstGame = false;
 
         }
+
     }
 
-    private boolean promptPlayAgain() {
+    /**
+     * This method prompts the player to see if they would like to play again.
+     *
+     * @param user This is the user being prompted.
+     * @return boolean true if the user wants to play again, false if the user is out of money
+     *                 or does not want to play again.
+     */
+    private boolean promptPlayAgain(Player user) {
         Scanner scanner = new Scanner(System.in);
+
+        if (user.getPotValue() == 0) {
+            return false;
+        }
 
         System.out.println("Would you like to play again? y/n");
 
         String response = scanner.next();
 
         if (response.equalsIgnoreCase("y")) {
+            user.printCurrentPot();
             return true;
         } else {
             System.out.println("Thank you for playing, good bye!");
@@ -68,15 +82,17 @@ public class BlackjackController {
         }
     }
 
-    private void promptComputerPlay(Deck deck, Player computer) {
+    private void promptComputerPlay(Deck deck, Player user, Player computer) {
         if (computer.cont) {
             if (computer.computerAI()) {
                 deck.deal(computer);
                 computer.printHand();
                 if (computer.getHand().bust()) {
                     System.out.println("The computer has busted!");
-                    computer.cont = false;
                     computer.getHand().busted = true;
+                    computer.cont = false;
+                    user.cont = false;
+                    determineWinner(user, computer);
 
                 }
             } else {
@@ -86,7 +102,7 @@ public class BlackjackController {
         }
     }
 
-    private void promptUserPlay(Deck deck, Player user) {
+    private void promptUserPlay(Deck deck, Player user, Player computer) {
         Scanner scanner = new Scanner(System.in);
         if (user.cont) {
             System.out.println("Would you like another card? y/n");
@@ -98,8 +114,10 @@ public class BlackjackController {
                 user.printHand();
                 if (user.getHand().bust()) {
                     System.out.println("You have busted!");
-                    user.cont = false;
                     user.getHand().busted = true;
+                    user.cont = false;
+                    computer.cont = false;
+                    determineWinner(user, computer);
                 }
             } else {
                 user.cont = false;
@@ -138,41 +156,51 @@ public class BlackjackController {
                 System.out.println("Your bet is too high, please bet less than your pot value!");
                 validInput = false;
             }
-        }while(validInput == false);
+        } while (validInput == false);
     }
 
 
-    public void determineWinner (Player user, Player computer){
-            if (user.getHand().busted) {
-                System.out.println("The computer has won!");
-                user.loseBet();
-                return;
-            } else if (computer.getHand().busted) {
-                System.out.println("You have won!");
-                user.winBet();
-                return;
-            } else if (computer.getHand().busted || user.getHand().busted) {
-                System.out.println("You and the computer have busted, you both lose!");
-                user.loseBet();
+    public void determineWinner(Player user, Player computer) {
+        if (user.getHand().busted) {
+            System.out.println("The computer has won!");
+            user.loseBet();
+            if (user.getHand().busted && user.getPotValue() == 0) {
+                System.out.println("You have lost all your money, the game is over!");
+                user.cont = false;
                 return;
             }
 
-            if (user.getHand().getScore() > computer.getHand().getScore()) {
-                System.out.println("You have won!");
-                user.winBet();
-            } else if (user.getHand().getScore() < computer.getHand().getScore()){
-                System.out.println("The computer has won!");
-                user.loseBet();
-            }
-            if (user.getHand().getScore() == computer.getHand().getScore()) {
-                System.out.println("It's a stalemate!");
-                user.staleBet();
-            }
-            if (user.getPotValue() == 0){
-                System.out.println("You have lost all your money, the game is over!");
-                java.lang.System.exit(0);
-            }
+        } else if (computer.getHand().busted) {
+            System.out.println("You have won!");
+            user.winBet();
+            return;
+
+        } else if (computer.getHand().busted || user.getHand().busted) {
+            System.out.println("You and the computer have busted, you both lose!");
+            user.loseBet();
+            return;
         }
+
+        if (user.getHand().getScore() > computer.getHand().getScore()) {
+            System.out.println("You have won!");
+            user.winBet();
+            return;
+
+        } else if (user.getHand().getScore() < computer.getHand().getScore()) {
+            System.out.println("The computer has won!");
+            user.loseBet();
+            return;
+        }
+        if (user.getHand().getScore() == computer.getHand().getScore()) {
+            System.out.println("It's a stalemate!");
+            user.staleBet();
+            return;
+        }
+//            if (user.getPotValue() == 0){
+//                System.out.println("You have lost all your money, the game is over!");
+//                java.lang.System.exit(0);
+//            }
+    }
 
 }
 
