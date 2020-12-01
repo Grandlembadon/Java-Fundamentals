329367af62cdbed385e0aafb7838b547c419f006
Liam Donovan <grandlembadon@gmail.com>
2020-11-30 23:37:17 -0500

    Committing lab work

---
+++ src/labs_examples/static_nonstatic/labs/ClassB.java
@@ -0,0 +1,10 @@
+package labs_examples.static_nonstatic.labs;
+
+public class ClassB {
+    public static void methodBingo(){
+        System.out.println("Bingo!!");
+    }
+    public void methodBongo(){
+        System.out.println("Bongo!!");
+    }
+}
