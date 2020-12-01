6910542342034578b291917802feb099a23f62bd
Liam Donovan <grandlembadon@gmail.com>
2020-12-01 00:17:00 -0500

    Committing lab work

---
+++ src/labs_examples/packages/labs/package1/PackClass1.java
@@ -0,0 +1,15 @@
+package labs_examples.packages.labs.package1;
+
+import labs_examples.packages.labs.package2.PackClass2;
+
+public class PackClass1 {
+    public void methodWhoo(){
+        System.out.println("Whoo!");
+
+        PackClass2 obj = new PackClass2();
+        obj.macAndCheese();
+    }
+    protected void methodWahoo(){
+        System.out.println("Wahoo!!");
+    }
+}
---
+++ src/labs_examples/packages/labs/package2/PackClass2.java
@@ -0,0 +1,15 @@
+package labs_examples.packages.labs.package2;
+
+import labs_examples.packages.labs.package1.PackClass1;
+
+public class PackClass2 {
+    public void sandwiches(){
+        System.out.println("I love sandwiches!");
+
+        PackClass1 obj = new PackClass1;
+        obj.methodWahoo();
+    }
+    protected void macAndCheese(){
+        System.out.println("I love Mac and Cheese!");
+    }
+}
