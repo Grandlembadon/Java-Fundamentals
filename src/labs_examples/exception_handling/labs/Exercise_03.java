464a03a6c9d378330a503b57b6929d06ec3de3ca
Liam Donovan <legrandelembali@Liams-iMac.local>
2020-12-04 15:12:34 -0500

    Committing lab work

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,18 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+class TryCatchFinal{
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+
+        try{
+            int x = nums[5];
+        } catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Exception was Caught!");
+        } finally {
+            System.out.println("The Program is finished!");
+        }
+
+    }
+}
