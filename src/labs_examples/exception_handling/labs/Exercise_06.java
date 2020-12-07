6a854b9a0cb0ca5aebea4e649d9f476cde941852
Liam Donovan <grandlembadon@gmail.com>
2020-12-06 22:14:48 -0500

    committing lab work

--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -6,4 +6,27 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate throwing an exception in one method and catching it in another method.
  *
  */
+class Throwdown {
+    public static void main(String[] args) {
+        try{
+            method();
+        } catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Exception caught from below...");
+        }
 
+
+    }
+    public static void method() throws ArrayIndexOutOfBoundsException {
+        int[] numz = {1,5,3,7};
+        try{
+            int x = numz[75];
+        } catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Exception caught!");
+            System.out.println("Throwing exception to main method...");
+            throw exc;
+
+        }
+
+    }
+
+}
--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -1,8 +1,55 @@
 package labs_examples.exception_handling.labs;
 
+import java.awt.*;
+
 /**
  * Exception Handling Exercise 7:
  *
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+class Slushee {
+    public static void main(String[] args) {
+        try {
+            slusheeMaster("Blue Raspberry");
+        } catch (BadFlavor a){
+            a.printStackTrace();
+        }
+
+        try {
+            slusheeMaster("Watermelon");
+        } catch (BadFlavor b){
+            b.printStackTrace();
+        }
+
+        try {
+            slusheeMaster("Mountain Dew");
+        } catch (BadFlavor c){
+            c.printStackTrace();
+        }
+
+
+
+    }
+    public static void slusheeMaster(String flavor) throws BadFlavor {
+        if (flavor.equalsIgnoreCase("Blue Raspberry")){
+            System.out.println("The ultimate flavor, Blue Raspberry. You chose well.");
+        } else if (flavor.equalsIgnoreCase("Watermelon")){
+            System.out.println("Watermelon eh, not a bad flavor at all.");
+        } else {
+            throw new BadFlavor(flavor);
+        }
+
+    }
+}
+class BadFlavor extends Exception {
+    String flavor;
+
+    public BadFlavor(String flavor){
+        this.flavor = flavor;
+    }
+    public String toString(){
+        return "Yuck, " + flavor + " is the worst possible flavor.";
+    }
+
+}
