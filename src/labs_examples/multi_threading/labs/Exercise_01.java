25c214bf2587ae702d18f13e6a0a26382c8d7a59
Liam Donovan <grandlembadon@gmail.com>
2020-12-14 22:05:20 -0500

    Committing lab work

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -8,4 +8,46 @@ package labs_examples.multi_threading.labs;
  *
  */
 
+class RunnableInterfaceExcercise {
+    public static void main(String[] args) {
+        System.out.println("Begin Main Thread");
+        MyRunnable runnable1 = new MyRunnable();
+        Thread thread1 = new Thread(runnable1, "Initiate1");
+        thread1.start();
 
+        Thread thread2 = new Thread(new MyRunnable(), "Initiate2");
+        thread2.start();
+
+
+    }
+}
+
+class MyRunnable implements Runnable {
+
+    Thread thread;
+
+    public MyRunnable(String name){
+        thread = new Thread(this,name);
+
+        thread.start();
+    }
+
+    public MyRunnable() {
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println(thread.getName() + " starting.");
+        try{
+            for(int count = 0; count <15; count++) {
+                Thread.sleep(200);
+                System.out.println("In " + thread.getName() + ", count is " + count);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thread.getName() + " terminating.");
+
+    }
+}
--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,23 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class ThreadController {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MyThread extends Thread {
+
+    MyThread(String name) {
+
+        super(name);
+
+        start();
+    }
+    @Override
+    public void run() {
+        System.out.println(getName() + " starting.");
+    }
+}
