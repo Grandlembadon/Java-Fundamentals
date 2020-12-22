a6f8531847c31ecc32bd2fd8bae6a826bd81ffcf
Liam Donovan <grandlembadon@gmail.com>
2020-12-21 21:32:38 -0500

    Committing lab work

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -11,46 +11,49 @@ package labs_examples.multi_threading.labs;
 class RunnableInterfaceExcercise {
     public static void main(String[] args) {
         System.out.println("Begin Main Thread");
-        MyRunnable runnable1 = new MyRunnable("High Priority");
-        MyRunnable runnable2 = new MyRunnable("Low Priority");
-
-        runnable1.thread.setPriority(Thread.NORM_PRIORITY+5);
-        runnable2.thread.setPriority(Thread.NORM_PRIORITY-4);
-
-        runnable1.thread.start();
-        runnable2.thread.start();
-
-
+//        MyRunnable runnable1 = new MyRunnable("High Priority");
+//        MyRunnable runnable2 = new MyRunnable("Low Priority");
+//
+//        runnable1.thread.setPriority(Thread.NORM_PRIORITY+5);
+//        runnable2.thread.setPriority(Thread.NORM_PRIORITY-4);
+//
+//        runnable1.thread.start();
+//        runnable2.thread.start();
+//
+        MyRunnable runnable = new MyRunnable();
+        Thread thread = new Thread(runnable, "Thread 1");
+        thread.start();
 
     }
 }
 
 class MyRunnable implements Runnable {
 
-    Thread thread;
 
-    public MyRunnable(String name){
-        thread = new Thread(this,name);
-
-
-    }
-
-    public MyRunnable() {
-
-    }
+//    Thread thread;
+//
+//    public MyRunnable(String name){
+//        thread = new Thread(this,name);
+//
+//
+//    }
+//
+//    public MyRunnable() {
+//
+//    }
 
     @Override
     public void run() {
-        System.out.println(thread.getName() + " starting.");
+        System.out.println(Thread.currentThread().getName() + " starting.");
         try{
             for(int count = 0; count <15; count++) {
                 Thread.sleep(200);
-                System.out.println("In " + thread.getName() + ", count is " + count);
+                System.out.println("In " + Thread.currentThread().getName() + ", count is " + count);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(thread.getName() + " terminating.");
+        System.out.println(Thread.currentThread().getName() + " terminating.");
 
     }
 }
