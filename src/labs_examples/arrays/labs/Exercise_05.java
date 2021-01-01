f04b49d75bb17b7677d019b8338527461cfa450a
Liam Donovan <grandlembadon@gmail.com>
2020-12-31 20:13:32 -0500

    Committing lab work

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -10,7 +10,7 @@ package labs_examples.arrays.labs;
 
 public class Exercise_05 {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         for (int i = array.length-1; i >= 1; i = i-2){
             System.out.println(array[i]);
         }
