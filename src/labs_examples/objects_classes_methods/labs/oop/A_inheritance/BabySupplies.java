4d53837c3a61c892d32a510ee7a33bb5bd3fe6e9
Liam Donovan <grandlembadon@gmail.com>
2021-01-14 21:38:14 -0500

    Committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/BabySupplies.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/BabySupplies.java
@@ -3,11 +3,18 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 public class BabySupplies {
 }
 class Diapers extends BabySupplies {
+    public Diapers(int totalDumpCapacity, String name, double price) {
+        this.totalDumpCapacity = totalDumpCapacity;
+        this.name = name;
+        this.price = price;
+    }
+
     int totalDumpCapacity;
     String name;
     double price;
 
 }
 class Pampers extends Diapers {
+
 
 }
