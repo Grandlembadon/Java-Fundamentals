93755008dc68d9f0ad5e4749ed387448cc903ffa
Liam Donovan <grandlembadon@gmail.com>
2021-01-18 22:11:16 -0500

    Committing lab work

--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -6,3 +6,57 @@ package labs_examples.multi_threading.labs;
  *      Write a program that will print 1-100 sequentially from at least two distinct threads. For instance, thread1 will
  *      print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
  */
+
+class TestClass {
+
+    private volatile Integer count = 1;
+    private volatile Integer threadIdToRun = 1;
+    private Object object = new Object();
+
+    public static void main(String[] args) {
+
+        TestClass testClass = new TestClass();
+        Thread t1 = new Thread(testClass.new Printer(1));
+        Thread t2 = new Thread(testClass.new Printer(2));
+
+
+        t1.start();
+        t2.start();
+    }
+
+    class Printer implements Runnable {
+
+        private int threadId;
+
+        public Printer(int threadId) {
+            super();
+            this.threadId = threadId;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (count <= 100) {
+                    synchronized (object) {
+                        if (threadId != threadIdToRun) {
+                            object.wait();
+                        } else {
+                            System.out.println("Thread " + threadId + " printed " + count);
+                            count += 1;
+
+                            if (threadId == 1)
+                                threadIdToRun = 2;
+                            else if (threadId == 2)
+                                threadIdToRun = 1;
+
+                            object.notifyAll();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
