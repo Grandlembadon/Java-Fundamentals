c7b59786190cb16e5beb1749c19d001ca2aa3553
Liam Donovan <grandlembadon@gmail.com>
2021-01-27 23:18:06 -0500

    Committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/DungeonBattleController.java
+++
@@ -1,7 +0,0 @@
-package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
-
-public class DungeonBattleController {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/DungeonBattlerController.java
@@ -0,0 +1,12 @@
+package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
+
+public class DungeonBattlerController {
+    public static void main(String[] args) {
+        DungeonBattlerController controller = new DungeonBattlerController();
+
+
+    }
+    public void playDungeonBattler() {
+
+    }
+}
