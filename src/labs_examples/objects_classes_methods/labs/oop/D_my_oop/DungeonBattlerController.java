0928a12d2d9c6703b65fd812c5dcd34567df7684
Liam Donovan <grandlembadon@gmail.com>
2021-01-28 23:56:03 -0500

    Committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/DungeonBattlerController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/DungeonBattlerController.java
@@ -1,14 +1,20 @@
 package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
 
+import java.util.Scanner;
+
 public class DungeonBattlerController {
     public static void main(String[] args) {
         DungeonBattlerController controller = new DungeonBattlerController();
+        controller.playDungeonBattler();
 
 
     }
     public void playDungeonBattler() {
         System.out.println("Welcome, brave adventurer. If you seek the riches that lie" +
-                "within this ancient dungeon, speak thy name and enter.");
+                " within this ancient dungeon, speak thy name and enter.");
+
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.next();
 
 
 
