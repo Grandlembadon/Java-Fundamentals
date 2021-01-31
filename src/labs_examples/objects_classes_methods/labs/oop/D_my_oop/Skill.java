85468c8f7c817ddde0bf2ccf3c08b5e47806139c
Liam Donovan <grandlembadon@gmail.com>
2021-01-30 19:40:21 -0500

    Committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Skill.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Skill.java
@@ -5,4 +5,36 @@ public class Skill {
     int [] skillDamageRange;
     int numberOfUses;
     String skillName;
+
+    public int getSkillCost() {
+        return skillCost;
+    }
+
+    public void setSkillCost(int skillCost) {
+        this.skillCost = skillCost;
+    }
+
+    public int[] getSkillDamageRange() {
+        return skillDamageRange;
+    }
+
+    public void setSkillDamageRange(int[] skillDamageRange) {
+        this.skillDamageRange = skillDamageRange;
+    }
+
+    public int getNumberOfUses() {
+        return numberOfUses;
+    }
+
+    public void setNumberOfUses(int numberOfUses) {
+        this.numberOfUses = numberOfUses;
+    }
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
 }
