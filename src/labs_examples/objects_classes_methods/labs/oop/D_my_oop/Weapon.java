2c9766e10f9650a7441dc7c27cc8f94f24e6bd0e
Liam Donovan <grandlembadon@gmail.com>
2021-01-30 19:39:25 -0500

    Committing lab work

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Weapon.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Weapon.java
@@ -5,7 +5,14 @@ public class Weapon {
     String damageType;
     int durability;
 
+    public Weapon(int[] damageRange, String damageType, int durability) {
+        this.damageRange = damageRange;
+        this.damageType = damageType;
+        this.durability = durability;
+    }
+
     public void strike(){
         
     }
+
 }
