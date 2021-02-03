09a9afb07f20d649b1d8220da885f8bc49abc055
Liam Donovan <grandlembadon@gmail.com>
2021-02-02 23:57:06 -0500

    Commiting lab work

--- src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Item.java
+++ src/labs_examples/objects_classes_methods/labs/oop/D_my_oop/Item.java
@@ -3,13 +3,20 @@ package labs_examples.objects_classes_methods.labs.oop.D_my_oop;
 public class Item {
     boolean isUsed;
     String itemName;
+
+    public Item(boolean isUsed, String itemName, int itemQuantity) {
+        this.isUsed = isUsed;
+        this.itemName = itemName;
+        this.itemQuantity = itemQuantity;
+    }
+
     int itemQuantity;
 
     public void useItem(){
 
     }
     public void discardItem(){
-        
+
     }
 
 
