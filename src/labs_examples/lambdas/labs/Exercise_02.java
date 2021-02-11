b75df47453c10666f3ceba7df2e10a93036bb20c
Liam Donovan <grandlembadon@gmail.com>
2021-02-10 23:09:22 -0500

    Committing lab work

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,5 +1,9 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.BiConsumer;
+import java.util.function.BooleanSupplier;
+import java.util.function.IntConsumer;
+
 /**
  * Lambdas Exercise 2:
  *
@@ -7,3 +11,18 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+class Test2 {
+    public static void main(String[] args) {
+        BiConsumer<Integer, Integer> subTwo = (x,y) -> System.out.println(x - y);
+        subTwo.accept(10,2);
+
+        BooleanSupplier tf = () -> true;
+        System.out.println(tf.getAsBoolean());
+
+        IntConsumer con = ()
+
+    }
+
+
+}
